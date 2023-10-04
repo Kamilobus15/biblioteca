cb9860d185b4ff08f21a5a318ba2d7ed0d85f47d
Camilo Bustos <bu_ivan@javeriana.edu.co>
2023-10-04 11:34:42 -0500

    ya funciona el tema de la interconección de las tablas, no como me gustaría pero funciona

--- libreria/api/models.py
+++ libreria/api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class Libro(models.Model):
     titulo = models.CharField(max_length=30)
-    autor = models.CharField(max_length=30)
-    editorial = models.CharField(max_length=30)
+    autor = models.ForeignKey('autor', on_delete=models.CASCADE, null=True)
+    editorial = models.ForeignKey('editorial', on_delete=models.CASCADE, null=True)
     genero = models.CharField(max_length=30)
     tipo = models.CharField(max_length=30)
 
--- libreria/libreria/settings.py
+++ libreria/libreria/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'libreria.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'dbLibreria',
+            'USER': 'root',
+            'PASSWORD': 'camilo2170',
+            'HOST': 'localhost',  # O la dirección IP de tu servidor MariaDB
+            'PORT': '3307',       # Puerto de MariaDB (por defecto es 3306)
+        }
 
 }
 
